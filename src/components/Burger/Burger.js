c44d91978bce93463d66d5962a915a9294aae13e
yowchun <yowchun93@gmail.com>
2018-05-13 16:45:55 +0800

    to display a message to user if user has no ingredients in the burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,12 +7,22 @@ const burger = (props) => {
     // why dont we comment how the structure of the object will actually become. 
     // from ingredients = { salad , bacon , cheese }
     // into [<BurgerIngredient key="bread-top1" type="bread-top" ... ]
-    const transformedIngredients = Object.keys(props.ingredients)
+    // and reduce it into a Big Array 
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             });
-        });
+        })
+        .reduce((arr,el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p> Please start adding ingredient! </p>
+    }
+    
+    console.log(transformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
