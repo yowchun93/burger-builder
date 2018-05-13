2fa822df646ba0f3fc0cb9979e9a4ca6a2c2e577
yowchun <yowchun93@gmail.com>
2018-05-13 15:04:59 +0800

    added Burger component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
