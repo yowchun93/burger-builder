2c7f544ec89acff46ff884daf5844c7efe408a4d
yowchun <yowchun93@gmail.com>
2018-05-13 16:59:50 +0800

    added BuildControl JS

---
+++ src/components/BuildControls/BuildControls.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Cheese', type: 'cheese'},
+    { label: 'Meat', type: 'meat'},
+];
+
+const buildControls = (props) => {
+    <div>
+    </div>
+}
+
+export default buildControls;
